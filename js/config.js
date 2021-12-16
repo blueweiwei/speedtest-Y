c1bcfbaac0a4e8fc6621bad1a1405e5b9f3bce93
weiwei <2849234292@qq.com>
2021-12-16 22:23:53 +0800

    edit config.js Signed-off-by: weiwei <“2849234292@qq.com”>

--- js/config.js
+++ js/config.js
@@ -1,20 +1,20 @@
 //LIST OF TEST SERVERS. See documentation for details if needed
 var SPEEDTEST_SERVERS=[
-	{	//this server doesn't actually exist, remove it
-		name:"***", //user friendly name for the server
-		server:"***", //URL to the server. // at the beginning will be replaced with http:// or https:// automatically
-		dlURL:"backend/garbage.php",  //path to download test on this server (garbage.php or replacement)
-		ulURL:"backend/empty.php",  //path to upload test on this server (empty.php or replacement)
-		pingURL:"backend/empty.php",  //path to ping/jitter test on this server (empty.php or replacement)
-		getIpURL:"backend/getIP.php"  //path to getIP on this server (getIP.php or replacement)
+	{	
+		name:"name", 
+		server:"//demo.demo.demo",
+		dlURL:"backend/garbage.php",
+		ulURL:"backend/empty.php", 
+		pingURL:"backend/empty.php",
+		getIpURL:"backend/getIP.php"
 	},
-	{	//this server doesn't actually exist, remove it
-		name:"***", //user friendly name for the server
-		server:"***", //URL to the server. // at the beginning will be replaced with http:// or https:// automatically
-		dlURL:"backend/garbage.php",  //path to download test on this server (garbage.php or replacement)
-		ulURL:"backend/empty.php",  //path to upload test on this server (empty.php or replacement)
-		pingURL:"backend/empty.php",  //path to ping/jitter test on this server (empty.php or replacement)
-		getIpURL:"backend/getIP.php"  //path to getIP on this server (getIP.php or replacement)
-	}
+	{	
+		name:"name", 
+		server:"//demo.demo.demo",
+		dlURL:"backend/garbage.php",
+		ulURL:"backend/empty.php", 
+		pingURL:"backend/empty.php",
+		getIpURL:"backend/getIP.php"
+	},
 	//add other servers here, comma separated
 ];
